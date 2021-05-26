9a8dab253d92670c39bb45b9831a7b51436880eb
dylanmeuriot <dylanmeuriot@gmail.com>
2021-05-26 18:02:45 +0200

    Etape 2 : imports and exports modules boot

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,12 +1,12 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-
+import { NgModule } from '@angular/core';
+import { IconsModule } from '../icons/icons.module';
+import { TemplatesModule } from '../templates/templates.module';
+import { UiModule } from '../ui/ui.module';
 
 @NgModule({
   declarations: [],
-  imports: [
-    CommonModule
-  ]
+  imports: [CommonModule],
+  exports: [UiModule, TemplatesModule, IconsModule],
 })
-export class CoreModule { }
+export class CoreModule {}
